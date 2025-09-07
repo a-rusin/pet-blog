d662c8193d800d9063c64fd480816874f01bd365
Alexandr Rusin <i@arusin.ru>
2025-09-08 00:24:45 +0700

    feat: добавил отображение пользователя в шапке

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,25 @@
 import { Link } from "react-router-dom";
 import { routes } from "../configs/routes";
+import { useAppSelector } from "../types/store";
 
 export const Header = () => {
+  const { user } = useAppSelector((state) => state.auth);
+
   return (
     <header className="container">
       <nav className="flex justify-between items-center h-20">
         <Link to={routes.home} className="font-cabinet-grotesk-variable text-3xl inline-block">
           My Blog
         </Link>
-        <Link to={routes.login} className="font-light text-black/50 text-xl hover:underline hover:decoration-solid">
-          Login
-        </Link>
+        {user ? (
+          <div className="font-light text-black/50 text-xl ">
+            Hello, <span className="underline decoration-solid">{user.login}</span>!
+          </div>
+        ) : (
+          <Link to={routes.login} className="font-light text-black/50 text-xl hover:underline hover:decoration-solid">
+            Login
+          </Link>
+        )}
       </nav>
     </header>
   );
